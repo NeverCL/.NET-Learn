87224a2ce2c91e72c182292a9ac88a188d47b44c
Never、C <caolin>
2017-02-20 17:31:12 +0800

    add di

---
+++ 02.DI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            // 注册泛型接口实现
+            serviceCollection.AddTransient(typeof(IFoo<>),typeof(Foo<>));
+            var provider = serviceCollection.BuildServiceProvider();
+            provider.GetService(typeof(IFoo<string>));
+            Console.ReadKey();
+        }
+    }
+
+    public class Foo<T> : IFoo<T>
+    {
+        public Foo()
+        {
+            System.Console.WriteLine("Foo执行构造函数");
+        }
+    }
+
+    public interface IFoo<T1>
+    {
+
+    }
+}
--- README.md
+++ README.md
@@ -13,3 +13,25 @@
 - dotnet 文件格式
     - project.json  --dotnetcore project files(类似csproject)
     - *.xproj       --通过vs打开dotnetcore 项目,会自动生成该文件
+
+
+## 02. DI
+
+- IServiceProvider
+    - 该接口提供唯一的方法GetService()
+    - Core的实现为ServiceProvider
+    - 通过IServiceCollection的BuildServiceProvider()获取实现
+
+- ServiceDescriptor
+    - 保存待实例化的接口和实现的关系,包括生命周期
+    - 生命周期:Singleton,Scoped,Transient
+
+- 服务的注册与获取
+    - 注册:本质就是添加ServiceDescriptor对象
+    - 获取:通过IServiceProvider接口即可获取对象
+
+- 注册泛型
+    - 
+
+
+
