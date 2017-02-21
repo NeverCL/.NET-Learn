f8d459447ec9ddb21545d23955b4269e37d0662f
Never、C <caolin>
2017-02-21 17:28:16 +0800

    fix  hello

--- 01.hello/Program.cs
+++ 01.hello/Program.cs
@@ -7,7 +7,10 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            // Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            // 下面这种方式也能解决中文乱码问题
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
             Console.WriteLine("Hello World!你好,世界!");
             Console.WriteLine(Console.ReadLine());
             Console.ReadKey();
