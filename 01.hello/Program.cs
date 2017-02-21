348d46b66fa9be4787f37e81f109828dc5174fb0
Never、C <caolin>
2017-02-21 17:18:27 +0800

    fix hello

--- 01.hello/Program.cs
+++ 01.hello/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApplication
 {
@@ -6,7 +7,9 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.WriteLine("Hello World!你好,世界!");
+            Console.WriteLine(Console.ReadLine());
             Console.ReadKey();
         }
     }
